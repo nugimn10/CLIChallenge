70317598521152fdd30645f6645017fe2f512557
nugimn10 <nugi.mn@gmail.com>
2020-02-12 21:50:48 +0700

    Revisi1

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Globalization;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace Latihan12Feb
+{
+    [HelpOption("--hlp")]
+    class Program
+    {
+        public static int Main(string[] args)
+
+          =>  CommandLineApplication.Execute<Program>(args);
+            
+        [Argument(0, Description = "Configuration file")]
+        [FileExists]
+        public string ConfigurationFile { get; }
+
+        [Argument(1, Description = "Comparison file 1")]
+        [FileExists]
+        public string ComparisonFile1 { get; }
+
+        [Argument(2, Description = "Comparison File 2")]
+        [FileExists]
+        public string ComparisonFile2 { get; }
+
+        private void OnExecute()
+        {
+            Console.WriteLine(ConfigurationFile);
+            Console.WriteLine(ComparisonFile1);
+            Console.WriteLine(ComparisonFile2);
+        }
+
+        // public class StringTranform 
+        // {
+        //      [Option("lowercase",Description = "The String")]
+        //     public string Subject { get; set;}
+        //     public void OnExecute()
+        //     {
+        //         var subject = Subject.ToLower() ;
+
+                
+        //     }
+        // } 
+
+        // public class StringTranform2 
+        // {
+        //      [Option("upercase",Description = "The String")]
+        //     public string Subject { get; set;}
+        //     public void OnExecute()
+        //     {
+        //         var subject = Subject.ToUpper() ;
+        //         Console.WriteLine($"Hello {subject}!");
+
+        //     }
+        // }
+        // public class StringTranform3 
+        // {
+        //      [Option("capitalize",Description = "The String")]
+        //     public string Subject { get; set;}
+        //     public void OnExecute()
+        //     {
+        //         var subject = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(Subject) ;
+        //         Console.WriteLine($"Hello {subject}!");
+
+        //     }
+        // }      
+    }
+     
+ }
+ 
+
